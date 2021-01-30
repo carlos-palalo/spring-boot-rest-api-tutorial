bb2c3aa3a615438f2aa7bcfd7abd91525ace7171
carlos-palalo <palaxcarlos@gmail.com>
2021-01-30 22:40:08 +0100

    Commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -7,24 +7,26 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+
 @Service
 @Transactional
 public class UserService {
-    @Autowired
-    private UserRepository userRepository;
-    public List<User> listAllUser() {
-        return userRepository.findAll();
-    }
+	@Autowired
+	private UserRepository userRepository;
 
-    public void saveUser(User user) {
-        userRepository.save(user);
-    }
+	public List<User> listAllUser() {
+		return userRepository.findAll();
+	}
 
-    public User getUser(Integer id) {
-        return userRepository.findById(id).get();
-    }
+	public void saveUser(User user) {
+		userRepository.save(user);
+	}
 
-    public void deleteUser(Integer id) {
-        userRepository.deleteById(id);
-    }
+	public User getUser(Integer id) {
+		return userRepository.findById(id).get();
+	}
+
+	public void deleteUser(Integer id) {
+		userRepository.deleteById(id);
+	}
 }
